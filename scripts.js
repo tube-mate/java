158fcc9e72c1731974a03408d2b6d29df0976cda
tube-mate <raangan23@gmail.com>
2024-11-12 20:23:38 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ window.onload = function () {
     select.after(contentDiv);
   
     // Fetch the text file
-    fetch("text/reactans.txt")
+    fetch("reactans.txt")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok " + response.statusText);
